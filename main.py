5254bb6f101f2c3aa58ff678bbcfec1ff6264e97
Drakx116 <alexandre2.guyot@gmail.com>
2020-06-17 11:45:34 +0200

    Add alive route

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from flask import jsonify
 from bin import app
 
 app = app.init()
+
+
+@app.route('/', methods={ 'GET' })
+def alive():
+    return jsonify({ 'message': 'Welcome on a Flask API.' })
+
 
 if __name__ == '__main__':
     app.run()
