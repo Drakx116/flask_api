42fb0a0436b4a2e70e0ae701bb92afa6c62ac3de
Drakx116 <alexandre2.guyot@gmail.com>
2020-06-17 14:19:08 +0200

    Add POST /auth/login route

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = app.init()
 
 @app.route('/', methods={ 'GET' })
 def alive():
-    return jsonify({ 'message': 'Welcome on a Flask API.' })
+    return jsonify({ 'message': 'Welcome on a Flask API.' }), 200
+
+
+@app.route('/auth/login', methods={ 'POST' })
+def login():
+    return jsonify({ 'message': 'Route not implemented yet.' }), 404
 
 
 if __name__ == '__main__':
